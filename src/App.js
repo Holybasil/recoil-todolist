ec31d9cdf12adff1025f0d2ac3d67275c6633270
holybasil <holybasil.1128@gmail.com>
2020-07-03 14:40:14 +0800

    add go back snapshot

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react";
-import { useResetRecoilState } from "recoil";
+import React, { useState } from "react";
+import {
+  useResetRecoilState,
+  useRecoilTransactionObserver_UNSTABLE,
+  useGotoRecoilSnapshot,
+} from "recoil";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { hot } from "react-hot-loader/root";
 import { setConfig } from "react-hot-loader";
-import { todoListSelector } from "./store";
+import { todoListState, todoListSelector } from "./store";
 import Home from "./Home";
 import Todo from "./Todo";
 import styles from "./Home/style.module.css";
@@ -13,7 +17,16 @@ setConfig({
 });
 
 const App = () => {
+  const [snapshots, setSnapshots] = useState([]);
+
   const resetTodoList = useResetRecoilState(todoListSelector);
+
+  useRecoilTransactionObserver_UNSTABLE(({ snapshot }) => {
+    setSnapshots([...snapshots, snapshot]);
+  });
+
+  // when we change the state by `gotoSnapshot`, a new snapshot is produced in fact
+  const gotoSnapshot = useGotoRecoilSnapshot();
 
   return (
     <div className={styles.wrapper}>
@@ -23,8 +36,21 @@ const App = () => {
           resetTodoList();
         }}
       >
-        重置TodoList
+        reset TodoList
       </button>
+      <div className={styles.backSnapshotButtonGroup}>
+        {snapshots.map((snapshot, index) => (
+          <button
+            className={styles.resetButton}
+            onClick={() => {
+              gotoSnapshot(snapshot);
+            }}
+          >
+            go back snapshot {index}
+          </button>
+        ))}
+      </div>
+
       <Router>
         <Switch>
           <Route path="/todo">
